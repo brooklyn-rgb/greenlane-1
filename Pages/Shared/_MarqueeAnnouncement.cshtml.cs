12f76cd74cef07dcbead033f186cd0bc94731b6e
brooklyn-rgb <brooklynstore151@gmail.com>
2026-01-15 01:27:06 -0800

    Fix: Resolved Git index.lock issue and ready for redeploy

---
+++ Pages/Shared/_MarqueeAnnouncement.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace greenlane.Pages.Shared
+{
+    public class _MarqueeAnnouncementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
